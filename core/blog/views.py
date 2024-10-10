24024b6b18eb2ae1bc8f95e7ab226d200b1613d2
akbar <akbarbareghe7@gmail.com>
2024-10-10 11:05:35 -0800

    [ADD] RedirectView

--- core/blog/views.py
+++ core/blog/views.py
@@ -4,13 +4,18 @@ from django.views.generic.base import TemplateView
 from .models import Post
 
 
-# Create your views here.
-def indexView(request):
+def index_view(request):
+    """
+    a function based view to show the index page.
+    """
     context = {"name": "ali"}
     return render(request, 'index.html', context=context)
 
 
 class IndexView(TemplateView):
+    """
+    a class  based view to show the index page.
+    """
     template_name = 'index.html'
 
     def get_context_data(self, **kwargs):

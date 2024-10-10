c81f35505a2382f53558d360d6a4ca97c932b05e
akbar <akbarbareghe7@gmail.com>
2024-10-10 15:26:02 -0800

    [ADD] RedirectView detail

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -7,8 +7,6 @@ from django.views.generic import RedirectView
 app_name = 'blog'
 
 urlpatterns = [
-    path('fbv-index', views.index_view, name='fbv-index'),
     path('cbv-index', views.IndexView.as_view(), name='cbv-index'),
-    path('redirect-to-google',RedirectView.as_view(url='https://google.com'),name='redirect-to-google'),
-    path('redirect-to-index',RedirectView.as_view(pattern_name='blog:cbv-index'),name='redirect-to-google'),
+    path('go-to-maktab/<int:pk>/',views.RedirectToMaktabView.as_view(), name='redirect-to-maktabkhooneh'),
 ]
--- core/blog/views.py
+++ core/blog/views.py
@@ -1,15 +1,8 @@
-from django.shortcuts import render
-from django.views.generic.base import TemplateView
+from django.shortcuts import render, get_object_or_404
+from django.views.generic.base import TemplateView, RedirectView
 
 from .models import Post
 
-
-def index_view(request):
-    """
-    a function based view to show the index page.
-    """
-    context = {"name": "ali"}
-    return render(request, 'index.html', context=context)
 
 
 class IndexView(TemplateView):
@@ -23,3 +16,13 @@ class IndexView(TemplateView):
         context['name'] = 'ali'
         context['posts'] = Post.objects.all()
         return context
+
+
+class RedirectToMaktabView(RedirectView):
+    url = 'https://maktabkhooneh.org'
+
+    def get_redirect_url(self, *args, **kwargs):
+        post = get_object_or_404(Post, pk=self.kwargs['pk'])
+        print(post)
+        return super().get_redirect_url(*args, **kwargs)
+

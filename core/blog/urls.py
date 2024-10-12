90c43742e4a1a9c1892630fee17447caef788209
beyramzadeh <akbarbareghe7@gmail.com>
2024-10-12 09:33:27 +0330

    [ADD] DeleteView

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path('blog/post/', views.PostList.as_view(), name='post-list'),
     path('post/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
     path('post/create/', views.PostCreateView.as_view(), name='post-create'),
-    path('post/<int:pk>/edit/', views.PostEditView.as_view(), name='post-update')
+    path('post/<int:pk>/edit/', views.PostEditView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
 ]
--- core/blog/views.py
+++ core/blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic.base import TemplateView, RedirectView
-from django.views.generic import ListView, DetailView, FormView, CreateView, UpdateView
+from django.views.generic import ListView, DetailView, FormView, CreateView, UpdateView, DeleteView
 
 from .forms import PostForm
 from .models import Post
@@ -43,6 +43,7 @@ class PostList(ListView):
 class PostDetailView(DetailView):
     model = Post
     template_name = 'blog/my_post_detail.html'
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         return context
@@ -65,31 +66,6 @@ class PostEditView(UpdateView):
     success_url = '/blog/post/'
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+class PostDeleteView(DeleteView):
+    model = Post
+    success_url = '/blog/post/'

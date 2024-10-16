402d87b3017531458b86e7eacfeea75da9a23e66
beyramzadeh <akbarbareghe7@gmail.com>
2024-10-16 16:19:22 +0330

    [ADD] delete request

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from .serializers import PostSerializer
 from ...models import Post
 from django.shortcuts import get_object_or_404
+from rest_framework import status
 
 
 @api_view(["GET", "POST"])
@@ -18,14 +19,17 @@ def post_list(request):
         return Response(serializer.data)
 
 
-@api_view(["GET", "PUT"])
+@api_view(["GET", "PUT", "DELETE"])
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk, status=True)
     if request.method == "GET":
         serializer = PostSerializer(post)
         return Response(serializer.data)
     elif request.method == "PUT":
-        serializer = PostSerializer(post,data=request.data)
+        serializer = PostSerializer(post, data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
+    elif request.method == "DELETE":
+        post.delete()
+        return Response({"detail": "item removed successfully"}, status=status.HTTP_204_NO_CONTENT)

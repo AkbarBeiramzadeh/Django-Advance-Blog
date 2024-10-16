ba797bf5cad9798e41e81951e941905fcfde28fe
beyramzadeh <akbarbareghe7@gmail.com>
2024-10-16 10:41:29 +0330

    [ADD] url

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -1,7 +1,17 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+data = {
+    "id": 1,
+    "title":"hello"
+}
+
 
 @api_view()
 def post_list(request):
     return Response("Ok")
+
+
+@api_view()
+def post_detail(request, id):
+    return Response(data)

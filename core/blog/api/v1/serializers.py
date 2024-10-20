e0760e44bc5605dec78ebf013cb007680c36aefb
beyramzadeh <akbarbareghe7@gmail.com>
2024-10-20 15:59:23 +0330

    [ADD] read_only_fields

--- core/blog/api/v1/serializers.py
+++ core/blog/api/v1/serializers.py
@@ -8,9 +8,16 @@ from ...models import Post, Category
 
 
 class PostSerializer(serializers.ModelSerializer):
+    # content = serializers.ReadOnlyField()
+    # or
+    # content = serializers.CharField(read_only=True)
+    # or
+    # appending to read_only_fields list in Meta class
+
     class Meta:
         model = Post
         fields = ('id', 'author', 'title', 'content', 'status', 'created_date', 'published_date')
+        read_only_fields = ['content']
 
 
 class CategorySerializer(serializers.ModelSerializer):

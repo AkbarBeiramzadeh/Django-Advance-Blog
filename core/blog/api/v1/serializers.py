a2a9f5699bb05bf86c3080dc1d7f79152560dee6
beyramzadeh <akbarbareghe7@gmail.com>
2024-10-21 16:47:10 +0330

    [ADD] SerializerMethodField

--- core/blog/api/v1/serializers.py
+++ core/blog/api/v1/serializers.py
@@ -10,7 +10,7 @@ from ...models import Post, Category
 class PostSerializer(serializers.ModelSerializer):
     snippet = serializers.ReadOnlyField(source='get_snippet')
     relative_url = serializers.URLField(source='get_absolute_api_url', read_only=True)
-    absolute_url = serializers.SerializerMethodField()
+    absolute_url = serializers.SerializerMethodField(method_name="get_abs_url")
 
     class Meta:
         model = Post
@@ -18,8 +18,9 @@ class PostSerializer(serializers.ModelSerializer):
             'id', 'author', 'title', 'content', 'snippet', 'relative_url', 'absolute_url', 'status', 'created_date',
             'published_date')
 
-    def get_absolute_url(self, obj):
-        return "test"
+    def get_abs_url(self, obj):
+        request = self.context.get('request')
+        return request.build_absolute_uri(obj.pk)
 
 
 class CategorySerializer(serializers.ModelSerializer):

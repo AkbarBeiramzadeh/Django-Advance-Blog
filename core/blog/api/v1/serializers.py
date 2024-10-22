486b00f2ebc67fe3568f37d3489748f8f5c3d719
beyramzadeh <akbarbareghe7@gmail.com>
2024-10-22 10:08:14 +0330

    [ADD] to_representation

--- core/blog/api/v1/serializers.py
+++ core/blog/api/v1/serializers.py
@@ -6,24 +6,32 @@ from ...models import Post, Category
 #     id = serializers.IntegerField()
 #     title = serializers.CharField(max_length=255)
 
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ('id', 'name',)
+
 
 class PostSerializer(serializers.ModelSerializer):
     snippet = serializers.ReadOnlyField(source='get_snippet')
     relative_url = serializers.URLField(source='get_absolute_api_url', read_only=True)
     absolute_url = serializers.SerializerMethodField(method_name="get_abs_url")
 
+    # category = serializers.SlugRelatedField(many=False, slug_field='name', queryset=Category.objects.all())
+    # category = CategorySerializer()
+
     class Meta:
         model = Post
         fields = (
-            'id', 'author', 'title', 'content', 'snippet', 'relative_url', 'absolute_url', 'status', 'created_date',
+            'id', 'author', 'title', 'content', 'category', 'snippet', 'relative_url', 'absolute_url', 'status',
+            'created_date',
             'published_date')
 
     def get_abs_url(self, obj):
         request = self.context.get('request')
         return request.build_absolute_uri(obj.pk)
 
-
-class CategorySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Category
-        fields = ('id', 'name',)
+    def to_representation(self, instance):
+        rep = super().to_representation(instance)
+        rep["category"] = CategorySerializer(instance.category).data
+        return rep

962e04ce56d2dbcb2f6272adb2ada8961b4bfd19
beyramzadeh <akbarbareghe7@gmail.com>
2024-10-22 10:51:16 +0330

    [ADD] to_representation

--- core/blog/api/v1/serializers.py
+++ core/blog/api/v1/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from ...models import Post, Category
+from accounts.models import Profile
 
 
 # class PostSerializer(serializers.Serializer):
@@ -23,15 +24,27 @@ class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = (
-            'id', 'author', 'title', 'content', 'category', 'snippet', 'relative_url', 'absolute_url', 'status',
-            'created_date',
-            'published_date')
+            'id', 'author', 'title', 'image', 'content', 'category', 'snippet', 'relative_url', 'absolute_url',
+            'status', 'created_date', 'published_date')
+        read_only_fields = ['author']
 
     def get_abs_url(self, obj):
         request = self.context.get('request')
         return request.build_absolute_uri(obj.pk)
 
     def to_representation(self, instance):
+        request = self.context.get('request')
         rep = super().to_representation(instance)
-        rep["category"] = CategorySerializer(instance.category).data
+        if request.parser_context.get('kwargs').get('pk'):
+            rep.pop('snippet', None)
+            rep.pop('relative_url', None)
+            rep.pop('absolute_url', None)
+        else:
+            rep.pop('content', None)
+
+        rep["category"] = CategorySerializer(instance.category, context={'request': request}).data
         return rep
+
+    def create(self, validated_data):
+        validated_data['author'] = Profile.objects.get(user__id=self.context.get('request').user.id)
+        return super().create(validated_data)

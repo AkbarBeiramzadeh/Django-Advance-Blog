7fd7e26848c05fd1d0d67605240c614cf0fc88b7
beyramzadeh <akbarbareghe7@gmail.com>
2024-10-28 12:56:42 +0330

    [ADD] jwt urls

--- core/core/settings.py
+++ core/core/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'django_filters',
     'drf_yasg',
+    'rest_framework_simplejwt',
 
 ]
 
@@ -143,6 +144,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ]
 
 }
--- core/core/urls.py
+++ core/core/urls.py
@@ -34,15 +34,15 @@ schema_view = get_schema_view(
         license=openapi.License(name="BSD License"),
     ),
     public=True,
-    permission_classes=[permissions.AllowAny,],
+    permission_classes=[permissions.AllowAny, ],
 )
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('accounts/', include('accounts.urls',namespace='accounts')),
+    path('accounts/', include('accounts.urls', namespace='accounts')),
     path('', include('blog.urls')),
-    path('api-docs/',include_docs_urls(title='api sample')),
+    path('api-docs/', include_docs_urls(title='api sample')),
     path('swagger/output.json', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),

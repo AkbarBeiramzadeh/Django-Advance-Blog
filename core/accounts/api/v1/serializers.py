f1ee29de51a87458351f21d8c532f9965ecf11c6
akbar <akbarbareghe7@gmail.com>
2024-10-27 21:21:21 -0800

    [ADD] RegistrationApiView

--- core/accounts/api/v1/serializers.py
+++ core/accounts/api/v1/serializers.py
@@ -1,16 +1,29 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
+from django.contrib.auth.password_validation import validate_password
+from django.core.exceptions import ValidationError
 
 User = get_user_model()
 
 
 class RegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, max_length=255, label='password', style={'input_type': 'password'})
+    password1 = serializers.CharField(write_only=True, max_length=255, label='confirm password', style={'input_type': 'password'})
+
     class Meta:
         model = User
-        fields = ('email', 'password')
+        fields = ('email', 'password', 'password1')
 
     def validate(self, attrs):
+        if attrs['password'] != attrs['password1']:
+            raise serializers.ValidationError({'detail': 'Passwords do not match'})
+
+        try:
+            validate_password(attrs.get('password'))
+        except ValidationError as e:
+            raise serializers.ValidationError({'password': list(e.messages)})
         return super().validate(attrs)
 
     def create(self, validated_data):
-        return super().create(validated_data)
+        validated_data.pop('password1', None)
+        return User.objects.create_user(**validated_data)

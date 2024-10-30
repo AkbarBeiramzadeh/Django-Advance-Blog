5bba724b8c127ec57acea845d0cdf4cf4ccad3c9
beyramzadeh <akbarbareghe7@gmail.com>
2024-10-30 11:36:50 +0330

    [ADD] email threading

--- core/accounts/api/v1/views.py
+++ core/accounts/api/v1/views.py
@@ -12,7 +12,8 @@ from rest_framework_simplejwt.views import TokenObtainPairView
 from django.contrib.auth import get_user_model
 from ...models import Profile
 
-from mail_templated import send_mail
+from mail_templated import EmailMessage
+from ..utils import EmailThread
 
 User = get_user_model()
 
@@ -93,5 +94,7 @@ class ProfileApiView(generics.RetrieveUpdateAPIView):
 
 class TestEmailSend(generics.GenericAPIView):
     def get(self, request, *args, **kwargs):
-        send_mail('email/hello.tpl', {'name': "ali"}, "admin@admin.com", ["test@test.com"])
+        email_obj = EmailMessage('email/hello.tpl', {'name': "ali"}, "admin@admin.com", to=["test@test.com"])
+        EmailThread(email_obj).start()
+
         return Response("email sent")
--- core/templates/email/hello.tpl
+++ core/templates/email/hello.tpl
@@ -6,4 +6,5 @@ Hello {{ name }}
 
 {% block html %}
 This is an <strong>html</strong> message.
+<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmCy16nhIbV3pI1qLYHMJKwbH2458oiC9EmA&s">
 {% endblock %}

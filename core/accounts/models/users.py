fd5509a4877eac0d0fbdc3d9df35a4661da257f7
beyramzadeh <akbarbareghe7@gmail.com>
2024-11-04 15:29:26 +0330

    [ADD] TestUrl

--- core/accounts/models/users.py
+++ core/accounts/models/users.py
@@ -5,10 +5,7 @@ from django.contrib.auth.models import (
     PermissionsMixin,
 )
 from django.utils.translation import gettext_lazy as _
-from django.db.models.signals import post_save
-from django.dispatch import receiver
 
-from django.contrib.auth import get_user_model
 
 
 class UserManager(BaseUserManager):
@@ -68,20 +65,4 @@ class User(AbstractBaseUser, PermissionsMixin):
         return self.email
 
 
-class Profile(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    first_name = models.CharField(max_length=250)
-    last_name = models.CharField(max_length=250)
-    image = models.ImageField(null=True, blank=True)
-    description = models.TextField()
 
-    created_date = models.DateTimeField(auto_now_add=True)
-    updated_date = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return self.user.email
-
-    @receiver(post_save, sender=User)
-    def save_profile(sender, instance, created, **kwargs):
-        if created:
-            Profile.objects.create(user=instance)
--- core/blog/tests.py
+++ core/blog/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from django.urls import reverse, resolve
 
-# Create your tests here.
+from blog.views import IndexView
+
+
+class TestUrl(TestCase):
+
+    def test_blog_index_url_resolve(self):
+        url = reverse('blog:cbv-index')
+        self.assertEquals(resolve(url).func.view_class, IndexView)

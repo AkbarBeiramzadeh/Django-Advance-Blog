d7a288466655325e24d6f57f73ac37e2d5e8bc90
beyramzadeh <akbarbareghe7@gmail.com>
2024-11-05 08:44:01 +0330

    [ADD] setUp

--- core/blog/tests/test_blog_model.py
+++ core/blog/tests/test_blog_model.py
@@ -6,15 +6,23 @@ from accounts.models.profiles import Profile
 
 
 class TestPostModel(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(email="test_user@email.com", password="1020")
+        self.profile = Profile.objects.create(
+            user=self.user,
+            first_name="ali",
+            last_name="bey",
+            description="test descr"
+        )
+
     def test_create_post_with_valid_data(self):
-        user = User.objects.create_user(email="test_user@email.com",password="1020")
-        profile = Profile.objects.create(user=user,first_name="ali",last_name="bey",description="test descr")
         post = Post.objects.create(
-            author=profile,
+            author=self.profile,
             title="test",
             content="test content",
             status=True,
             category=None,
             published_date=datetime.now()
         )
+        self.assertTrue(Post.objects.filter(pk=post.id).exists())
         self.assertEquals(post.title, "test")

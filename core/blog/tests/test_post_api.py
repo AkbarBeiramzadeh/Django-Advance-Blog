1c51fb8f98cc6d94dde630479f609153e18ec887
beyramzadeh <akbarbareghe7@gmail.com>
2024-11-05 15:27:27 +0330

    [ADD] test_create_post_response_401_status

--- core/blog/tests/test_post_api.py
+++ core/blog/tests/test_post_api.py
@@ -3,10 +3,12 @@ from rest_framework.test import APIClient
 import pytest
 
 from accounts.models.users import User
+from datetime import datetime
 
 
 @pytest.mark.django_db
 class TestPostApi:
+    client = APIClient()
 
     def test_get_post_response_200_status(self):
         self.user = User(email="foo@bar.com")  # NB: You could also use a factory for this
@@ -15,11 +17,19 @@ class TestPostApi:
         self.user.save()
 
         # Authenticate client with user
-        self.client = APIClient()
+
         self.client.login(email=self.user.email, password=password)
         url = reverse("blog:api-v1:post-list")
         response = self.client.get(url)
         assert response.status_code == 200
 
     def test_create_post_response_401_status(self):
-        pass
+        url = reverse("blog:api-v1:post-list")
+        data = {
+            "title": "test",
+            "content": "description",
+            "status": "ToDo",
+            "published_date": datetime.now()
+        }
+        response = self.client.post(url, data)
+        assert response.status_code == 401

69154dd43dd61e9aba4e3a5f1a977cb7c67a3a04
beyramzadeh <akbarbareghe7@gmail.com>
2024-11-18 09:56:19 +0330

    celery

---
+++ core/locust/locustfile.py
@@ -0,0 +1,12 @@
+from locust import HttpUser, task, between
+
+
+class QuickstartUser(HttpUser):
+
+    @task
+    def post_list(self):
+        self.client.get("/api/v1/post/")
+
+    @task
+    def post_category(self):
+        self.client.get("/api/v1/category/")

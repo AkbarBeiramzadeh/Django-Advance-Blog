b53e3b75164093c8d18b4aa52ff2a0164709d074
beyramzadeh <akbarbareghe7@gmail.com>
2024-11-19 13:21:50 +0330

    cache_page

--- core/accounts/views.py
+++ core/accounts/views.py
@@ -1,8 +1,16 @@
-from django.http import HttpResponse
-import time
+from django.http import HttpResponse, JsonResponse
 from .tasks import send_email_task
+from django.core.cache import cache
+import requests
+
+from django.views.decorators.cache import cache_page
 
 
 def send_email(request):
     send_email_task.delay()
     return HttpResponse("<h1>Done Sending</h1>")
+
+@cache_page(60)
+def test(request):
+    response = requests.get("url")
+    return JsonResponse(response.json())
